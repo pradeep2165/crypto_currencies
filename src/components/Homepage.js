22fd3b68c22b6ef9ffb2a304796f91aa3e94dd72
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-11-08 13:37:25 +0530

    data formated with millify

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,6 @@
-import { Typography, Row, Col, Statistic } from 'antd'
 import React from 'react'
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd'
 import { useGetCryptosQuery } from '../services/cryptoApi';
 const {Title} = Typography;
 
@@ -13,11 +14,11 @@ const Homepage = () => {
       <Title level={2} className="heading">Global Crypto Stats</Title>
       <Row gutter={[32, 32]}>
         <Col span={12}><Statistic title="Total Cryptocurrencies"  value={globalStats.total}/></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value={globalStats.totalExchanges} /></Col>
-        <Col span={12}><Statistic title="Total Market Cap:"  value={globalStats.totalMarketCap}/></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value={globalStats.total24hVolume}/></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:"  value={`$${millify(globalStats.totalMarketCap)}`}/></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`}/></Col>
         <Col span={12}><Statistic title="Total coins"  value={globalStats.totalCoins}/></Col>
-        <Col span={12}><Statistic title="Total Markets"  value={globalStats.totalMarkets}/></Col>
+        <Col span={12}><Statistic title="Total Markets"  value={millify(globalStats.totalMarkets)}/></Col>
       </Row>
     </div>
   )
